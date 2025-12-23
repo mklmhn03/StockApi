aef3b4896413fe024ac1a36059a02df71aca5eec
mklmhn03 <mklmhn03@gmail.com>
2025-12-22 21:59:20 -0500

    basic server hosting

--- Program.cs
+++ Program.cs
@@ -1,41 +1,20 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapGet("/", () => "Stock API is running");
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("/stock/{symbol}", async (string symbol, IHttpClientFactory clientFactory) => 
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    return new 
+    { 
+        Status = "Online", 
+        ServerTime = DateTime.UtcNow, 
+        Version = "0.1",
+        Symbol = symbol
+    };
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
